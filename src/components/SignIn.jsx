b6737741b4597b9109701fe90be59b50b4d377eb
SANDRA-S-MENON <sandramenon2002@gmail.com>
2024-06-13 14:06:48 +0530

    navbar

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const SignIn = () => {
   return (
     <div>
+        <NavBar/>
         <h1><center>SignIn</center></h1>
         <div className="container">
             <div className="row">
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const SignUp = () => {
   return (
     <div>
+        <NavBar/>
         <h1><center>SignUp</center></h1>
         <div className="container">
             <div className="row">
